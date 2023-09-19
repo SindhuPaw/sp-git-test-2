d06bd5b9c72d958cf70fe34116d17ddfb5477fcc
SindhuPaw <sindhupawitra@gmail.com>
2023-09-19 14:48:12 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo error quis consectetur? Itaque cumque laborum dolore. Sed praesentium distinctio quam reiciendis, placeat necessitatibus nemo similique, vel, pariatur illum expedita rem?</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
